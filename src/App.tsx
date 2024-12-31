b8d374d9f43bc54c7c2172b7bb9235ddcb70a6e7
Artem <strikerlutsk@gmai.comuser.email>
2024-12-31 15:44:51 +0100

    rename navbar temp to navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import Navbar from "./scenes/navbar"
+import Navbar from "@/scenes/navbar"
 import Home from "@/scenes/home"
 
-import { useEffect, useState } from "react"
+import { useE
+  ffect, useState } from "react"
 import { SelectedPage } from "@/shared/types"
 import Benefits from "@/scenes/benefits"
 import OurClasses from "@/scenes/ourClasses"
